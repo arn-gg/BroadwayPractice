5bd23774be877a9d0f162b62e056a94a72865416
arn-gg <arun73138@gmail.com>
2026-04-27 09:02:18 +0545

    Completed

---
+++ day 3.py
@@ -0,0 +1,53 @@
+# # # print(True and True)
+# # # print(2==2 and 1>=1)
+
+# # # print("Test"=="hello" and True)
+
+
+# # logged_in = False
+# # print(not(logged_in))
+
+
+# # if(2==2):
+# #     print("this is true"):
+# # else:
+# #     print("this is not true block")
+    
+
+# score=int(input("Please enter you marks"))
+
+# if(score<0 or score>100):
+#     print("Please enter Valid Number")
+    
+# elif score<50:
+#     print("You've Failed")
+    
+# elif score>50 and score<60:
+#     print("2nd Div");
+    
+# elif score>60 and score<80:
+#     print("1st Div")
+    
+# elif score>80 and score<=100:
+#     print("Dist Div")
+    
+# else:
+#     print("please enter your Score")
+    
+    
+a=[1,2,3,4,5]
+b=['a','b','c','d']   
+# print(type(a))
+# data = ["hello",1,True,1.5]
+# print (data)
+# print (data[0])
+
+# print(len(data))
+
+a.append(6)
+# a.insert(2,"c")
+# a.extend(b)
+# print(a)
+
+c=a+b
+print(c)
---
+++ test.py
@@ -0,0 +1,4 @@
+print("hello world")
+
+username=input("what is your name?")
+print(username)
