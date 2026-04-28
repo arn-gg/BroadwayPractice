b9f5a5c506102e465c78dd163f1cae4fe3247134
arn-gg <arun73138@gmail.com>
2026-04-28 07:17:11 +0545

    dictonarylearn

---
+++ dictonary.py
@@ -0,0 +1,16 @@
+data= {
+    "name":"Sudan",
+    "age":21,
+    "address":"Nepal",
+    "salary":  15.4
+     }
+
+print(data)
+print(type(data))
+print(len(data))
+#all dictonary data types are mutable
+#same name but different kaam, polymorphism
+print(data["salary"])
+print(data["name"])
+
+
